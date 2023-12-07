b13ab380efc0f6fd7a3a78a308bd265f645a77db
Colin Pham <ctpham6@dons.usfca.edu>
2023-12-06 23:27:06 -0800

    FINAL!! Not the best code but I finished on time

--- IRoadTrip.java
+++ IRoadTrip.java
@@ -4,15 +4,15 @@ import java.util.*;
 import java.io.*;
 
 public class IRoadTrip {
-	worldMap map = new worldMap();
-	String start = "";
-	String end = "";
-	String input = "";
-	ArrayList<String> result;
+	worldMap map = new worldMap();            // THE WORLD MAP HAS 
+	String start = "";						// The starting country (STR)
+	String end = "";						// The desired destination (STR)
+	String input = "";						// User input
+	ArrayList<String> result;				//
     public IRoadTrip (String [] args) {
         // Replace with your code
     	
-    	if (args.length < 3) {
+    	if (args.length < 3) {								// Strict on there being 3 inputs
     		System.err.println("Not Enough Inputs");
     		System.exit(0);
     	} else if (args.length > 3){
@@ -20,9 +20,9 @@ public class IRoadTrip {
     		System.exit(0);
     	}
     	
-    	try {
-    		
-    		File borders = new File(args[0]);
+    	try {												// 1. Reads the border file
+    														// I load up the countries and their
+    		File borders = new File(args[0]);				// borders here. Aliases are also handled
     		
     		Scanner ciaCountries = new Scanner(borders);
     		
@@ -41,12 +41,12 @@ public class IRoadTrip {
     		System.exit(0);
     	}
     	
-    	map.aliasPolish();
-    	
+    	map.aliasPolish();									// Handles any names with parentheses () or a/b characters
+    														// Basically just polishing up the look of the aliases
     	try {
-    		File stateName = new File(args[2]);
-    		Scanner stateIDs = new Scanner(stateName);
-    		String idLine = stateIDs.nextLine();
+    		File stateName = new File(args[2]);				// 2. Read the state_name file
+    		Scanner stateIDs = new Scanner(stateName);		// Associate every country with a code
+    		String idLine = stateIDs.nextLine();			// Makes sure to avoid the expired countries
     		
     		while (stateIDs.hasNextLine()) {
     			idLine = stateIDs.nextLine();
@@ -59,9 +59,9 @@ public class IRoadTrip {
     		System.exit(0);
     	}
     	
-    	try {
-    		File capitalDistances = new File(args[1]);
-    		Scanner capDist = new Scanner(capitalDistances);
+    	try {													// 3. Read capdist file
+    		File capitalDistances = new File(args[1]);			// If a country is a border of another, log their cap distances
+    		Scanner capDist = new Scanner(capitalDistances);	// Ignore the extraneous ones (US to Iceland? No need!)
     		String distLine = capDist.nextLine();
     		
     		while (capDist.hasNextLine()) {
@@ -77,22 +77,22 @@ public class IRoadTrip {
     }
 
 
-    public int getDistance (String country1, String country2) {
+    public int getDistance (String country1, String country2) {					// Gets distances of 2 adjacent countries
         if ((map.find(country1) == true) && (map.find(country2) == true)) {
-        	if (map.isBorder(country1, country2) == true) {
-        		return(map.getDistance(country1, country2));
+        	if (map.isBorder(country1, country2) == true) {				// Is it a border?
+        		return(map.getDistance(country1, country2));			// Yes? getDistance
         	} else {
-        		return -1;
+        		return -1;												// No? return -1
         	}
         } else {
-        	return -1;	
+        	return -1;
         }
     }
 
 
-    public List<String> findPath (String country1, String country2) {
-        List<String> toReturn = (map.travel(country1, country2, "Give Me List"));
-        map.resetTravel();
+    public List<String> findPath (String country1, String country2) {						// NOTE: I did this method LAST which I realized I should not have done. I basically made overloaded methods
+        List<String> toReturn = (map.travel(country1, country2, "Give Me List"));			// of my existing methods so get me a list in return instead of just printing them out
+        map.resetTravel();																	// sorry
         return (toReturn);
     }
 
@@ -100,7 +100,7 @@ public class IRoadTrip {
     public void acceptUserInput() {
         // Replace with your code
     	Scanner userInput = new Scanner(System.in);
-    	System.out.println("Enter the name of the first country (type EXIT to quit): ");
+    	System.out.println("Enter the name of the first country (type EXIT to quit): ");			// User inputted section. Checks for invalid names. Exits when "EXIT" is typed
     	input = userInput.nextLine();
     	while (! input.equals("EXIT")) {
     		while ((map.find(input) == false) && (! input.equals("EXIT"))){
@@ -128,7 +128,7 @@ public class IRoadTrip {
     		}
     		end = input;
     		
-    		map.travel(start,end);
+    		map.travel(start,end);																						// Map class handles this
     		map.resetTravel();
     		System.out.println("Enter the name of the first country (type EXIT to quit): ");
     		input = userInput.nextLine();
@@ -148,10 +148,10 @@ public class IRoadTrip {
 
 }
 
-class worldMap{
-	
+class worldMap{																											// Welcome to the World Map!
+																														// It has a countryList filled with user-defined object: Object
 	List<Country> countryList = new ArrayList<>();
-	static int distance = 0;
+
 	
 	public void add(String countryStr){
 		
@@ -170,9 +170,9 @@ class worldMap{
 		
 		if ((alias != null) && (alias.contains("KEELING"))){
 			alias = "KEELING ISLANDS";
-			countryName = "COCOS ISLANDS";
+			countryName = "COCOS ISLANDS";						// Cocos island is weird. If/Else bandage
 		} else if (alias != null) {
-			countryName = countryName.split(" \\(")[0];
+			countryName = countryName.split(" \\(")[0];        // Gets part not in parentheses:  abcdefg (hijklmnop)
 		}
 		
 		try {
@@ -192,8 +192,8 @@ class worldMap{
 		
 	}
 	
-	public void aliasPolish() {
-		for (Country c : countryList) {
+	public void aliasPolish() {								// Some names are like  "Pham, Colin"
+		for (Country c : countryList) {						// The polish turns it into "Colin Pham"
 			if (c.name.contains(",")) {
 				String newAlias = "";
 				newAlias += c.name.split(", ")[1] + " ";
@@ -203,16 +203,16 @@ class worldMap{
 		}
 	}
 	
-	public void couToCod(String codeLine){
+	public void couToCod(String codeLine){					// Associates countries to code
 		String[] analyzer = codeLine.split("	");
 		String backUp = "";
 		String stateID = analyzer[1];
 		String correspondingCountry = analyzer[2];
 		String endDate = analyzer[4];
 		
-		if (stateID.equals("IND")){
-			for (Country c: countryList) {
-				if (c.name.equals("INDIA")) {
+		if (stateID.equals("IND")){							// Bandaged if/else statements
+			for (Country c: countryList) {					// for codes that did not pass the
+				if (c.name.equals("INDIA")) {				// automated system I have below
 					c.setID(stateID);
 					return;
 				}
@@ -247,9 +247,9 @@ class worldMap{
 			}
 		}
 		
-		if (endDate.equals("2020-12-31")) {
-			for (Country c: countryList) {
-				
+		if (endDate.equals("2020-12-31")) {				// If the country still exists...
+			for (Country c: countryList) {				// For every country, c in the countryList...
+				// A buffer, where the IDs will be assigned to the wrong countries unless I skip it
 				if ((stateID.equals("DOM")) && (c.name.equals("DOMINICA")) || (stateID.equals("PNG")) && (c.name.equals("GUINEA")) || (stateID.equals("GNB")) && (c.name.equals("GUINEA")) || (stateID.equals("SOM")) && (c.name.equals("MALI")) || (stateID.equals("NIG")) && (c.name.equals("NIGER"))){
 					
 				} else if ((c.name.contains(correspondingCountry.toUpperCase())) || (correspondingCountry.toUpperCase().contains(c.name))) {
@@ -264,9 +264,9 @@ class worldMap{
 						} catch (ArrayIndexOutOfBoundsException AIOOBE) {
 							
 						}
-						if (correspondingCountry.equals("Myanmar (Burma)")) {
-							correspondingCountry = "Myanmar";
-						} else if (correspondingCountry.equals("Myanmar (Burma)")) {
+						if (correspondingCountry.equals("Myanmar (Burma)")) {			// Myanmar is weird because usually:
+							correspondingCountry = "Myanmar";							// REAL NAME (ALIAS)
+						} else if (correspondingCountry.equals("Myanmar (Burma)")) {	// but ALIAS (REAL NAME)
 							correspondingCountry = "Myanmar";
 						}
 
@@ -275,6 +275,7 @@ class worldMap{
 
 					}
 					break;
+					// For the countries and aliases that have no correlation in spelling whatsoever: they were left for bandaged if/else statements
 				} else if ((c.name.equals("GERMANY")) && (correspondingCountry.equals("German Federal Republic")) || (c.name.equals("ESWATINI")) && (correspondingCountry.equals("Swaziland")) || (c.name.equals("KOREA, NORTH")) && (correspondingCountry.equals("Korea, People's Republic of")) || (c.name.equals("KOREA, SOUTH")) && (correspondingCountry.equals("Korea, Republic of")) || (c.name.equals("TIMOR-LESTE")) && (correspondingCountry.equals("East Timor")) || (c.name.equals("KYRGYZSTAN")) && (correspondingCountry.equals("Kyrgyz Republic")) || (c.name.equals("CZECHIA")) && (correspondingCountry.equals("Czech Republic")) || (c.name.equals("NORTH MACEDONIA")) && (correspondingCountry.equals("Macedonia (Former Yugoslav Republic of)")) || (c.name.equals("BOSNIA AND HERZEGOVINA")) && (correspondingCountry.equals("Bosnia-Herzegovina")) || (c.name.equals("ROMANIA")) && (correspondingCountry.equals("Rumania")) || (c.name.equals("CABO VERDE")) && (correspondingCountry.equals("Cape Verde")) || (c.name.equals("COTE D'IVOIRE")) && (correspondingCountry.equals("Cote D’Ivoire")) || (c.name.equals("CONGO, DEMOCRATIC REPUBLIC OF THE")) && (correspondingCountry.equals("Congo, Democratic Republic of (Zaire)"))) {
 					c.setID(stateID);
 					c.addAlias(correspondingCountry.toUpperCase());
@@ -284,7 +285,7 @@ class worldMap{
 		}
 	}
 	
-	public void setCapDist(String distLine){
+	public void setCapDist(String distLine){		// Sets distance from starting country's capital to border countries' capitals
 		String startCoun = distLine.split(",")[1];
 		String endCoun = distLine.split(",")[3];
 		String distanceStrKM = distLine.split(",")[4];
@@ -299,16 +300,16 @@ class worldMap{
 					}
 				}
 				
-				c.addCapDist(endCoun, distKM);
+				c.addCapDist(endCoun, distKM);  //endCoun = endCountry or the destination country if we were going border to border
 			}
 		}
 	}
 	
-	public boolean isStranded(String country) {
+	public boolean isStranded(String country) {								// Is it stranded? If it is, then there is no hope to reach the island
 		return countryList.get(countryList.indexOf(country)).stranded;
 	}
 	
-	public boolean isBorder(String c1, String c2) {
+	public boolean isBorder(String c1, String c2) {							// Are two countries bordering?
 		Country country1 = null;
 		Country country2 = null;
 		int found = 0;
@@ -343,7 +344,7 @@ class worldMap{
 		return true;
 	}
 	
-	public int getDistance(String country1, String country2) {
+	public int getDistance(String country1, String country2) {										// Gets distance of adjacent countries
 		Country c1 = null;
 		Country c2 = null;
 		for (Country c : countryList) {
@@ -378,7 +379,7 @@ class worldMap{
 		return -1;
 	}
 	
-	public ArrayList travel(String start, String end) {
+	public ArrayList travel(String start, String end) {												// The main method
 		Country startC = null;
 		Country endC = null;
 		
@@ -386,7 +387,7 @@ class worldMap{
 		
 		endC = getEntry(end);
 		
-		if (startC == endC) {
+		if (startC == endC) {															// If the inputs are the same, there is no need to travel
 			System.out.println("* " + startC.name + " --> " + endC.name + " (0km.)");
 			return null;
 		} else if (isBorder(start, end) == true) {
@@ -395,17 +396,17 @@ class worldMap{
 		}
 		
 		
-		if ((startC.stranded == true) || (endC.stranded == true)) {
+		if ((startC.stranded == true) || (endC.stranded == true)) {					// Checks for stranded islands
 			ArrayList<String> welp = new ArrayList<>();
 			System.out.println("Countries Can Not Be Reached");
 			return(null);
 			
 		}
 		
-		return(dijkstra(startC, endC));
+		return(dijkstra(startC, endC));					// Calls Dijkstra's algorithm
 	}
 	
-	public List<String> travel(String start, String end, String mode) {
+	public List<String> travel(String start, String end, String mode) {		// For the findPath method. Basically the same but returns a List<String>
 		Country startC = null;
 		Country endC = null;
 		
@@ -432,7 +433,7 @@ class worldMap{
 		return(dijkstra(startC, endC, mode));
 	}
 	
-	public ArrayList dijkstra(Country A, Country B) {
+	public ArrayList dijkstra(Country A, Country B) {			// First dijkstra method that starts with A to look at its borders
 		A.known = "true";
 		Country borderToCheck;
 		for (int i = 0; i < A.borders.size(); i++) {
@@ -442,10 +443,10 @@ class worldMap{
 			}
 		}
 		
-		return getPathing(A,B);
+		return getPathing(A,B);												// Calls an overloaded method to look at borders of borders
 	}
 	
-	public List<String> dijkstra(Country A, Country B, String mode) {
+	public List<String> dijkstra(Country A, Country B, String mode) {		// For findPath method
 		A.known = "true";
 		Country borderToCheck;
 		for (int i = 0; i < A.borders.size(); i++) {
@@ -458,10 +459,10 @@ class worldMap{
 		return getPathing(A,B,mode);
 	}
 	
-	public void dijkstra(Country A, String Origin, Integer cost) {
-		A.known = "true";
-		A.path = Origin;
-		A.setCost(cost, Origin);
+	public void dijkstra(Country A, String Origin, Integer cost) {						// The overloaded method				OVERARCHING Plan: Take starting country A
+		A.known = "true";																							// Find lowest cost of getting to every country IT CAN REACH
+		A.path = Origin;																							// Returns the cost of the destination country
+		A.setCost(cost, Origin);																					// HOWEVER, if the end country doesnt have a path back to A, then the two can not be reached
 		Country borderToCheck;
 		for (int i = 0; i < A.borders.size(); i++) {
 			borderToCheck = getEntry(A.borders.get(i));
@@ -477,15 +478,15 @@ class worldMap{
 		
 	}
 	
-	public ArrayList getPathing(Country A, Country B) {
+	public ArrayList getPathing(Country A, Country B) {													// Puts together the output and pathing from what has been put into the arrayList
 		
 		if (B.path.equals("")) {
 			System.err.println("A path between " + A.name + " and " + B.name + " is not possible");
-			return null;
+			return null;	// Is it reachable? Does it have a path back?
 		}
 		
 		ArrayList<String> pathing = new ArrayList<>();
-		Country medium = B;
+		Country medium = B;	// Temp variable so that I can go through the path and get each Country's cost and names
 		String befName = "";
 		int befCost = 0;
 		String medName = "";
@@ -518,14 +519,14 @@ class worldMap{
 			} else if (i == pathing.size()) {
 				System.out.println("* " + A.name + " --> " + befName + " (" + befCost + " km.)");
 			} else {
-				System.out.println("* " + medName + " --> " + befName + " (" + befCost + " km.)");
+				System.out.println("* " + medName + " --> " + befName + " (" + befCost + " km.)");				// Prints out pathing and costs. Accounts for edge cases
 			}
 		}
 		
 		return pathing;
 	}
 	
-	public List<String> getPathing(Country A, Country B, String mode) {
+	public List<String> getPathing(Country A, Country B, String mode) {										// Pathing for findPath but instead of printing, it .adds to the List<String> and returns
 		
 		if (B.path.equals("")) {
 			System.err.println("A path between " + A.name + " and " + B.name + " is not possible");
@@ -574,7 +575,7 @@ class worldMap{
 		return finalPath;
 	}
 	
-	public boolean find(String country) {
+	public boolean find(String country) {																// Is there a country linked to what's been inputted?
 		country = country.toUpperCase();
 		for (Country c : countryList) {
 			if ((c.name.equals(country.toUpperCase())) || (country.equals(c.name.toUpperCase()))) {
@@ -590,7 +591,7 @@ class worldMap{
 		return (false);
 	}
 	
-	public Country getEntry(String term) {
+	public Country getEntry(String term) {														// Inputs String "France" gets object Country FRANCE
 		for (Country c : countryList) {
 			if ((c.name.equals(term.toUpperCase())) || (term.equals(c.name.toUpperCase()))) {
 				return(c);
@@ -605,7 +606,7 @@ class worldMap{
 		return(null);
 	}
 	
-	public void resetTravel() {
+	public void resetTravel() {									// resets "table" so that another search can be done
 		for (Country c: countryList) {
 			c.known = "false";
 			c.cost = -1;
@@ -613,28 +614,25 @@ class worldMap{
 		}
 	}
 	
-	private Hashtable borders() {
-		return null;
-	}
 }
 
-class Country{
+class Country{										// Country class
 	
-	String name;
-	String uniqueID;
-	List<String> alias = new ArrayList<>();
-	List<String> borders = new ArrayList<>();
-	Boolean stranded = false;
+	String name;									// Each country has a name: United States
+	String uniqueID;								// ID: USA
+	List<String> alias = new ArrayList<>();			// Aliases: America
+	List<String> borders = new ArrayList<>();		// Borders: Mexico and Canada
+	Boolean stranded = false;						// Is it an island? false
 	
-	String known = "false";
+	String known = "false";							// Dijkstra table values
 	int cost = -1;
 	String path = "";
 	
-	Hashtable<String, Integer> capitalDistances = new Hashtable<>();
+	Hashtable<String, Integer> capitalDistances = new Hashtable<>();		// The distances of a country's capital to its borders' capitals
 	
 	Country(String entry, String otherEntry, String borderString){
 		
-		if (entry.equals("MICRONESIA, FEDERATED STATES OF")) {
+		if (entry.equals("MICRONESIA, FEDERATED STATES OF")) {			// BANDAGED if/else statements since not every place in borders.txt is associated with an ID or cost
 			name = "MICRONESIA";
 		} else {
 			name = entry;
@@ -656,15 +654,13 @@ class Country{
 			name = "MACEDONIA";
 			alias.add("NORTH MACEDONIA");
 		}
-		String borderName = "";
-		int borderDistance = -1;
 		
 		if (borderString == null) {
-			stranded = true;
+			stranded = true;									// If the country has no borders, it's stranded
 		} else {
 			String counToAdd = "";
-			String[] borderList = borderString.split("; ");
-			for (int i = 0; i < borderList.length; i++) {
+			String[] borderList = borderString.split("; ");			// The border adder. Extracts the [COUNTRY NAME] number km.
+			for (int i = 0; i < borderList.length; i++) {			// form the string
 				String[] analyzer = borderList[i].split(" ");
 				for (int j = 0; j < analyzer.length; j++) {
 					try {
@@ -703,8 +699,8 @@ class Country{
 	public void addBorder(String border){
 		
 		if (name.equals("CANADA") && (border.equals("Denmark")) || (name.equals("MOROCCO")) && (border.equals("Spain")) || ((border.equals("Gaza Strip"))) || (border.equals("Kosovo")) || (border.equals("Andorra")) || ((name.equals("DENMARK")) && (border.equals("Canada 1.3"))) || (border.equals("Holy See")) || (border.equals("Sardinia")) || (border.equals("Monaco")) || (border.equals("Gibraltar 1.2")) || (border.equals("Liechtenstein")) || (border.equals("West Bank")) || (border.equals("South Sudan"))) {
-			return;
-		} else if (border.equals("Botswana 0.15")) {
+			return;			// Removes these places since they either have no cost, ID, or they were just assigned borders that make no sense (Canada and Denmark)
+		} else if (border.equals("Botswana 0.15")) {		// Typos my code above didn't catch
 			border = "Botswana";
 		} else if (border.equals("Zambia 0.15")) {
 			border = "Zambia";
@@ -712,12 +708,12 @@ class Country{
 		borders.add(border);
 	}
 	
-	public void setCost(int toSet) {
+	public void setCost(int toSet) {		// Sets cost
 		cost = toSet;
 	}
 	
 	public void setCost(int toSet, String newPath) {
-		if ((toSet < cost) || (cost == -1)) {
+		if ((toSet < cost) || (cost == -1)) {		// Challenged pre-existing cost and changed path if it wins (lower than original)
 			cost = toSet;
 			path = newPath;
 		}
